c2a89923671e3c2cb30549b55dcecf56f0f7ea37
Udhaya Kumar R <YOUR_GITHUB_EMAIL>
2026-09-12 15:43:26 +0530

    Fix Vercel skill icons

--- src/routes/about/Stack.jsx
+++ src/routes/about/Stack.jsx
@@ -1,25 +1,52 @@
 import './Stack.sass';
 
-import SocialMediaIcon from '../../assets/images/skills/social-media.svg';
-import SeoIcon from '../../assets/images/skills/seo.svg';
-import AnalyticsIcon from '../../assets/images/skills/analytics.svg';
-import MetaIcon from '../../assets/images/skills/meta.svg';
-import CanvaIcon from '../../assets/images/skills/canva.svg';
-import JavaIcon from '../../assets/images/skills/java.svg';
-import CssIcon from '../../assets/images/skills/css.svg';
-import MysqlIcon from '../../assets/images/skills/mysql.svg';
-import JdbcIcon from '../../assets/images/skills/jdbc.svg';
+import SocialMediaIcon from "../../assets/images/skills/social-media.svg";
+import SeoIcon from "../../assets/images/skills/seo.svg";
+import AnalyticsIcon from "../../assets/images/skills/analytics.svg";
+import MetaIcon from "../../assets/images/skills/meta.svg";
+import CanvaIcon from "../../assets/images/skills/canva.svg";
+import JavaIcon from "../../assets/images/skills/java.svg";
+import HtmlCssIcon from "../../assets/images/skills/css.svg";
+import MysqlIcon from "../../assets/images/skills/mysql.svg";
+import JdbcIcon from "../../assets/images/skills/jdbc.svg";
 
-const stack = [
-  ['Social Media Marketing', SocialMediaIcon],
-  ['SEO', SeoIcon],
-  ['Google Analytics', AnalyticsIcon],
-  ['Meta Ads', MetaIcon],
-  ['Canva', CanvaIcon],
-  ['Java', JavaIcon],
-  ['HTML & CSS', CssIcon],
-  ['MySQL', MysqlIcon],
-  ['JDBC', JdbcIcon],
+const skills = [
+  {
+    name: "Social Media Marketing",
+    icon: SocialMediaIcon,
+  },
+  {
+    name: "SEO",
+    icon: SeoIcon,
+  },
+  {
+    name: "Google Analytics",
+    icon: AnalyticsIcon,
+  },
+  {
+    name: "Meta Ads",
+    icon: MetaIcon,
+  },
+  {
+    name: "Canva",
+    icon: CanvaIcon,
+  },
+  {
+    name: "Java",
+    icon: JavaIcon,
+  },
+  {
+    name: "HTML & CSS",
+    icon: HtmlCssIcon,
+  },
+  {
+    name: "MySQL",
+    icon: MysqlIcon,
+  },
+  {
+    name: "JDBC",
+    icon: JdbcIcon,
+  },
 ];
 
 export const Stack = () => {
